f559c56d9550ed9c558afe8e46000d9357b57f61
surelybrackets <bunch43@gmail.com>
2020-06-24 22:50:34 -0400

    setup new inspector app

--- src/apps/index.ts
+++ src/apps/index.ts
@@ -1,2 +1,3 @@
 export * as historicalData from '@surelybrackets/inspector-f-apps_historical-data'
 export * as movementStatistics from '@surelybrackets/inspector-f-apps_movement-statistics'
+export * as inspectors from '@surelybrackets/inspector-f-apps_inspectors'
